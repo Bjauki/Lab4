9430b2ab53e9cf7c537198f8fbd4947058c6b492
Bjauki <bialy9923@gmail.com>
2018-05-26 12:55:02 +0200

    LAB5 COMMIT #4 - finished add new item

--- app/src/main/java/com/example/patryk/lab4/MainActivity.java
+++ app/src/main/java/com/example/patryk/lab4/MainActivity.java
@@ -52,9 +52,13 @@ public class MainActivity extends AppCompatActivity {
     {
         if(requestCode==1 && resultCode==RESULT_OK)
         {
+
             Bundle extras = data.getExtras();
-            String nowy = (String)extras.get("wpis");
-            target.add(nowy);
+            Animal nowy = (Animal)extras.getSerializable("nowy");
+            //String nowy = (String)extras.get("wpis");
+            //target.add(nowy);
+            this.db.dodaj(nowy);
+            adapter.changeCursor(db.lista());
             adapter.notifyDataSetChanged();
         }
     }
